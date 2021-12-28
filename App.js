8770c127db7ac7c84f78117ff7f7599217bc6bab
Kapish Singh <megacap27@gmail.com>
2021-12-28 11:11:37 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Homepage from "./src/Homepage"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>NEWS APP</Text>
+      <Homepage/>
       <StatusBar style="auto" />
     </View>
   );
